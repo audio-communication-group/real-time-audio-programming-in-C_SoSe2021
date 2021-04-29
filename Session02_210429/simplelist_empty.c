946ab126e2efa15fdd1b48789bca1e9739e97016
Harvey Keitel <funker@posteo.net>
2021-04-29 16:15:19 +0200

    added simple list empty

---
+++ Session02_210429/simplelist_empty.c
@@ -0,0 +1,54 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct listnode
+{
+    int data;
+    struct listnode *next;
+} listnode;
+
+listnode *listnode_new(int data)
+{
+
+}
+
+typedef struct list
+{
+    listnode *head;
+    listnode *tail;
+} list;
+
+list *list_new(void)
+{
+
+}
+
+void list_append(list *x, int data)
+{
+
+}
+
+void list_prepend(list *x, int data)
+{
+
+}
+
+void list_print(list *x)
+{
+
+}
+
+int main()
+{
+
+    
+    return 0;
+}
